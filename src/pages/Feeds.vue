7a5806c812b47d6914e9e0276311db11f78b780d
Syed Mohammed Kamran <syedmohammed.kamran@factset.com>
2018-02-07 16:11:20 +0530

    Minor changes

--- src/pages/Feeds.vue
+++ src/pages/Feeds.vue
@@ -1,40 +1,11 @@
 <template>
-  <div>
-    <ul>
-      <li v-for="post in posts">{{post.userID}} : {{post.content}}</li>
-    </ul>
-    <el-input placeholder="Enter Content" v-model="post.content"></el-input>
-    <el-input placeholder="Enter Date of publishing" v-model="post.date"></el-input>
-    <button @click="addObject">Click ME</button>
-  </div>
+  <div>Feeds</div>
 </template>
 
 <script>
-  import {postsRef, userRef} from '../middleware/firebase';
-
-  export default {
-    name: "feeds",
-    data() {
-      return {
-        post: {
-          content: '',
-          date: ' ',
-          dislike: 0,
-          likes: 0,
-          userID: 100
-        }
-      };
-    },
-    firebase: {
-      posts: postsRef,
-      users: userRef
-    },
-    methods: {
-      addObject() {
-        postsRef.push(this.post);
-      }
+    export default {
+        name: "feeds"
     }
-  }
 </script>
 
 <style scoped>
--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -1,5 +1,5 @@
 <template>
-
+  <div>Profile</div>
 </template>
 
 <script>
